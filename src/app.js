c20a23011072de8fa9db4c71b876eb4f862d4328
mim-nikoo <134829937+mim-nikoo@users.noreply.github.com>
2023-06-04 01:20:25 -0700

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-alert("Hello love");
+//alert("Hello love");
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "b7fb433c7fddfa4844t24380290bd0do";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={bern}&key=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
