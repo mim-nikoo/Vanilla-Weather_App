0284b6d1c8aef65e1b35a53e7d086602a9811a22
mim-nikoo <134829937+mim-nikoo@users.noreply.github.com>
2023-06-05 00:07:16 -0700

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -42,8 +42,21 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.condition.description);
 }
-let apiKey = "b7fb433c7fddfa4844t24380290bd0do";
-let city = "tokyo";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+
+function search(city) {
+  let apiKey = "b7fb433c7fddfa4844t24380290bd0do";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
